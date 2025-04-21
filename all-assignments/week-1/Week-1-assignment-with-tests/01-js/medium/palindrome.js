9e9741e39055263a7bedfcedc640231597a1dd80
vishal-patidar-02 <vishalpatidar07070707@gmail.com>
2025-04-21 16:18:41 +0530

    Palindrome test pass

--- all-assignments/week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
+++ all-assignments/week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
@@ -6,8 +6,43 @@
   - `npm run test-palindrome`
 */
 
-function isPalindrome(str) {
-  return true;
+function reverseStr(str) {
+  let ReverseString = "";
+  for (let i = str.length-1; i >= 0; i--) {
+    ReverseString += str[i];
+  }
+  return ReverseString;
 }
 
+function trim(str) {
+  let string = "";
+  for (let i = 0; i <= str.length-1; i++) {
+    if (
+      str[i] == " " ||
+      str[i] == "?" ||
+      str[i] == "*" ||
+      str[i] == "," ||
+      str[i] == "." ||
+      str[i] == "@" ||
+      str[i] == "'" ||
+      str[i] == "!" 
+    ) {
+    } else {
+      string += str[i];
+    }
+  }
+  return string;
+}
+
+function isPalindrome(str) {
+  let Sentance = trim(str.toLowerCase());
+  if (Sentance === reverseStr(Sentance)) {
+    return true;
+  } else {
+    // console.log(Sentance);
+    return false;
+  }
+}
+
+// isPalindrome("'Able, was I ere I saw Elba!'");
 module.exports = isPalindrome;
