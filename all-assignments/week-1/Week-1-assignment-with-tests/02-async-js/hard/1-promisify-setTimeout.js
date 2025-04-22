3e1e7acab6e31ffeeafe6a14c0bcdaa9a957a62f
vishal-patidar-02 <vishalpatidar07070707@gmail.com>
2025-04-22 16:15:18 +0530

    1-promisify-setTimeout Solved

--- all-assignments/week-1/Week-1-assignment-with-tests/02-async-js/hard/1-promisify-setTimeout.js
+++ all-assignments/week-1/Week-1-assignment-with-tests/02-async-js/hard/1-promisify-setTimeout.js
@@ -3,4 +3,21 @@
 */
 
 function wait(n) {
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("The promise is resolved");
+    }, n);
+    if (!n) {
+      reject(" The Promise is rejected");
+    }
+  });
+  return myPromise;
 }
+
+wait()
+  .then((msg) => {
+    console.log(msg);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
