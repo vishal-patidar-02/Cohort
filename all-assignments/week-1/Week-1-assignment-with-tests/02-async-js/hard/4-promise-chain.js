6a2edbb7e512fcf0abada6bdb10d5952f0ac2c35
vishal-patidar-02 <vishalpatidar07070707@gmail.com>
2025-04-22 17:10:45 +0530

    4-promise-chain solved

--- all-assignments/week-1/Week-1-assignment-with-tests/02-async-js/hard/4-promise-chain.js
+++ all-assignments/week-1/Week-1-assignment-with-tests/02-async-js/hard/4-promise-chain.js
@@ -5,18 +5,54 @@
  * Compare it with the results from 3-promise-all.js
  */
 
-function waitOneSecond() {
 
+let start = new Date().getTime();
+
+function waitOneSecond() {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res("One Second promise is resolved");
+    }, 1000);
+  });
 }
 
 function waitTwoSecond() {
-
+  return new Promise((res) => {
+    setTimeout(() => {
+      res("Two Second promise is resolved");
+    }, 2000);
+  });
 }
 
 function waitThreeSecond() {
-
+  return new Promise((res) => {
+    setTimeout(() => {
+      res("Three Second promise is resolved");
+    }, 3000);
+  });
 }
 
 function calculateTime() {
-
+  waitOneSecond()
+    .then((msg1) => {
+      console.log(msg1);
+      return waitTwoSecond();
+    })
+    .then((msg2) => {
+      console.log(msg2);
+      return waitThreeSecond();
+    })
+    .then((msg3) => {
+      console.log(msg3);
+      let end = new Date().getTime();
+      console.log(
+        `Time taken by this to resolve is ${(end - start) / 1000} seconds`
+      );
+    })
+    .catch((err) => {
+      console.log("Error:", err);
+    });
 }
+
+calculateTime();
+
