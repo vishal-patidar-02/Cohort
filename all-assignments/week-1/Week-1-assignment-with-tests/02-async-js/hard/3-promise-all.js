8fb4f8bd3c2ba1375961e3525d5c20bddbf8c4a5
vishal-patidar-02 <vishalpatidar07070707@gmail.com>
2025-04-22 16:57:34 +0530

    3-promise-all solved

--- all-assignments/week-1/Week-1-assignment-with-tests/02-async-js/hard/3-promise-all.js
+++ all-assignments/week-1/Week-1-assignment-with-tests/02-async-js/hard/3-promise-all.js
@@ -4,19 +4,44 @@
  * Print how long it took for all 3 promises to resolve.
  */
 
-
 function waitOneSecond() {
-
+  return new Promise((res) => {
+    setTimeout(() => {
+      res("One Second promise is resolved");
+    }, 1000);
+  });
 }
 
 function waitTwoSecond() {
-
+  return new Promise((res) => {
+    setTimeout(() => {
+      res("Two Second promise is resolved");
+    }, 2000);
+  });
 }
 
 function waitThreeSecond() {
-
+  return new Promise((res) => {
+    setTimeout(() => {
+      res("Three Second promise is resolved");
+    }, 3000);
+  });
 }
 
 function calculateTime() {
+    let start = new Date().getTime();
 
+  Promise.all([waitOneSecond(), waitTwoSecond(), waitThreeSecond()])
+    .then((msg) => {
+      console.log(msg);
+      let end = new Date().getTime();
+      console.log(`Time taken by this to resolve is ${(end-start)/1000} seconds`)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+    
 }
+
+calculateTime();
