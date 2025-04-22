f014dbcd1107df5d41c80abdd98783079fb6bb84
vishal-patidar-02 <vishalpatidar07070707@gmail.com>
2025-04-22 10:38:12 +0530

    todo-list test passed

--- all-assignments/week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
+++ all-assignments/week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
@@ -12,7 +12,47 @@
 */
 
 class Todo {
+  constructor(){
+    this.ToDo = [];
+  }
+  add(task){
+     this.ToDo.push(task);
+  }
+  remove(indexOfTodo){
+    this.ToDo = this.ToDo.filter((task) => (task !== this.ToDo[indexOfTodo]));
+  }
+  update(index, updatedTodo){
+    if(!this.ToDo[index]){
+      return ;
+    }
+    this.ToDo[index] = updatedTodo;
+  }
+  getAll(){
+    return this.ToDo
+  }
+  get(indexOfTodo){
+    if(this.ToDo[indexOfTodo] === undefined){
+      return null
+    }
+    return this.ToDo[indexOfTodo]
+  }
+  clear(){
+    this.ToDo = [];
+  }
 
 }
+
+const tasks = new Todo();
+tasks.add("Eat");
+tasks.add("food");
+tasks.add("code");
+console.log(tasks.getAll());
+tasks.remove(1);
+console.log(tasks.getAll());
+tasks.update(3, "Sleep");
+console.log(tasks.getAll());
+console.log(tasks.get(0));
+tasks.clear();
+console.log(tasks.getAll());
 
 module.exports = Todo;
